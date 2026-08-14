ac8b2e73a3412e264bf1b8411723115b548cfffa
K <otgoonzayaamarsanaaa@gmail.com>
2026-08-14 21:45:37 +0800

    Change Supabase key and add error handling
    Updated Supabase key to use ANON_KEY and added error handling for missing environment variables.

--- src/lib/supabase-client.js
+++ src/lib/supabase-client.js
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
-
-export const supabaseClient = () =>
-  createClient(
-    supabaseUrl,
-    supabaseKey,
-  );        
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL болон Key олдсонгүй. Environment Variables-аа шалгана уу.");
+}
+export const supabase = createClient(supabaseUrl, supabaseKey);     
